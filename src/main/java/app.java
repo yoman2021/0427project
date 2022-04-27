6b722f4316fee5d51a5b9f9cadfef70dd1d241ab
ncyu <as9909022@gmail.com>
2022-04-27 10:34:52 +0800

    add initial fail

---
+++ src/main/java/app.java
@@ -0,0 +1,10 @@
+public class app {
+
+    public static void main(String[] args) {
+        GradeConverter converter = new GradeConverter();
+
+        for(int i=0; i< 105; i++) {
+            System.out.println(i+";"+converter.convert(i));
+        }
+    }
+}
